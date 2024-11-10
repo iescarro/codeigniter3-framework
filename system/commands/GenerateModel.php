845dcabb0be22299ce10014c3085ce97bf55e64b
Ian <ian.escarro@gmail.com>
2024-11-10 18:54:08 +0800

    Add generate model command

---
+++ system/commands/GenerateModel.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * CodeIgniter3
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2024, CodeIgniter Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter3
+ * @author	CodeIgniter3 Team
+ * @copyright	Copyright (c) 2024, CodeIgniter3 Team (https://github.com/iescarro/codeigniter3-framework)
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ * @link	https://github.com/iescarro/codeigniter3-framework
+ * @since	Version 1.0.0
+ * @filesource
+ */
+
+namespace CodeIgniter3\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateModel extends Command
+{
+  protected static $defaultName = 'generate:model';
+
+  protected function configure()
+  {
+    $this
+      ->setName('generate:model')
+      ->setDescription('')
+      ->addArgument('component', InputArgument::REQUIRED, 'The component to run (e.g., model)');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output): int
+  {
+    $component = $input->getArgument('component');
+
+    $generator = new Generator($component, array());
+    $generator->generate_model($output);
+
+    return Command::SUCCESS;
+  }
+}
